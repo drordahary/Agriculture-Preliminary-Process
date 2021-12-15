905297c1606fe6767539586dc106c61c8fc441ee
GilZoarets <gilzoarets@gmail.com>
2021-12-15 10:22:28 +0200

    Cultivations list's length is being automatically updated now.

--- src/MysqlCon.java
+++ src/MysqlCon.java
@@ -27,7 +27,7 @@ class MysqlCon{
         return null;
     }
 
-    private final static String url="jdbc:mysql://localhost:3306/agricalture_database?serverTimezone=UTC&verifyServerCertificate=false&useSSL=true&requireSSL=true";
+    private final static String url="jdbc:mysql://localhost:3306/agriculture_database?serverTimezone=UTC&verifyServerCertificate=false&useSSL=true&requireSSL=true";
     private static LinkedList <Cultivation> Cultivations = new LinkedList<>();
     public MysqlCon() {
     }
@@ -45,20 +45,22 @@ class MysqlCon{
             ResultSet rs;
             ResultSet soil;
             ResultSet growthType;
-            for (int i=1; i<=2; i++){
+            int n;
+            for (n=1; stmt.executeQuery("select * from cultivations where cult_id="+n).last(); n++);
+            for (int i=1; i<=n; i++){
                 rs=stmt.executeQuery("select * from cultivations where cult_id="+i);
                 soil = stmt2.executeQuery("select s.name from soils s inner join cultivations c on s.soil_id=c.soil_id where cult_id="+i);
                 growthType = stmt3.executeQuery("select g.name from growthtypes g inner join cultivations c on g.growthType_id=c.soil_id where cult_id="+i);
                 while(rs.next() && soil.next() && growthType.next())
                     Cultivations.add(new Cultivation(rs.getInt("cult_id"),rs.getString("name"), rs.getInt("water"),rs.getInt("temperature"),translatorSoil(soil.getString("name")), translatorType(growthType.getString("name"))));
-                Cultivation cult = Cultivations.get(i-1);
-                System.out.println(cult.toString());
+
 
 
            }
             con.close();
-
+            System.out.println(Cultivations.toString());
         }catch(Exception e){ System.out.println(e);}
+
 
     }
 }
